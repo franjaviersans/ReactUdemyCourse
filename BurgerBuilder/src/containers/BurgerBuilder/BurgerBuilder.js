cc4438859297b5c0102546d3d8e56aa6900adcd3
Francisco Sans <franjaviersans@gmail.com>
2018-06-13 09:00:34 -0400

    Adding global error handling component

--- BurgerBuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ BurgerBuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React, {Component } from 'react';
 
 import Aux from '../../hoc/AuxHOC/AuxHOC';
-import Burger from '../../components/Burger/Burger'
+import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from  '../../hoc/withErrorHandler/withErrorHandler';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -169,4 +170,4 @@ class BurgerBuilder extends Component{
 	}
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
