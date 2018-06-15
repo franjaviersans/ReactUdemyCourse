d3443d17ca2d3b6f3b433d39bbe7a7542c65c836
Francisco Sans <franjaviersans@gmail.com>
2018-06-15 15:04:20 -0400

    Adding some routes in the project to create a multi-page application

--- BurgerBuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ BurgerBuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,6 +37,7 @@ class BurgerBuilder extends Component{
 		axios.get('https://react-my-burger-3b4f9.firebaseio.com/ingredients.json')
 			.then(response => {				
 				this.setState({ ingredients: response.data });
+				this.updatePurchaseState(response.data);
 			})
 			.catch(error => {
 				this.setState({ error: true });
@@ -103,7 +104,7 @@ class BurgerBuilder extends Component{
 	purchaseContinuehandler = () => {
 		// alert('You continue!');
 
-		this.setState({
+		/*this.setState({
 			loading: true,
 		});
 
@@ -134,6 +135,20 @@ class BurgerBuilder extends Component{
 					purchasing: false,
 				});
 			});
+			*/
+
+		const queryParams = [];
+
+		for(let i in this.state.ingredients){
+			queryParams.push(encodeURIComponent(i)+'='+encodeURIComponent(this.state.ingredients[i]));
+		}
+
+		const queryString = queryParams.join('&');
+
+		this.props.history.push({
+			pathname: '/checkout',
+			search: '?'+queryString,
+		});
 	}
 
 	render () {
