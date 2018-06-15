13d7dca1517daeea9deaa85028bd1b8af8df4a8b
Francisco Sans <franjaviersans@gmail.com>
2018-06-15 09:02:11 -0400

    Adding route for courses and users. Specific link for courses passing parameters

---
+++ assignment3/src/containers/Course/Course.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+	
+	/*componentWillMount(){
+		console.log('Did mount');
+		console.log(this.props);
+	}*/
+
+    render () {
+        return (
+            <div>
+                <h1>_COURSE_TITLE_</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>>
+            </div>
+        );
+    }
+}
+
+export default Course;
---
+++ assignment3/src/containers/Courses/Courses.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import './Courses.css';
+import { withRouter } from 'react-router-dom';
+
+
+class Courses extends Component {
+    state = {
+        courses: [
+            { id: 1, title: 'Angular - The Complete Guide' },
+            { id: 2, title: 'Vue - The Complete Guide' },
+            { id: 3, title: 'PWA - The Complete Guide' }
+        ]
+    }
+
+    handleRoute = (id) => {
+        console.log(id);
+        this.props.history.push(
+            {
+              pathname: "/courses/"+id,
+              state: { some: 'state' }
+            }
+        );
+
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Amazing Udemy Courses</h1>
+                <section className="Courses">
+                    {
+                        this.state.courses.map( course => {
+                            return <article 
+                                className="Course" 
+                                key={course.id}
+                                onClick={() => this.handleRoute(course.id)}>
+                                {course.title} 
+                                </article>;
+                        } )
+                    }
+                </section>
+            </div>
+        );
+    }
+}
+
+export default withRouter(Courses);
