aaa5d17c9b7da9f6e4dc4cce83db006cf7eadabb
Francisco Sans <franjaviersans@gmail.com>
2018-07-21 12:06:40 +0100

    Finish testing NavigationItems

--- BurgerBuilder/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ BurgerBuilder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -11,15 +11,35 @@ configure({adapter: new Adapter()});
 
 
 describe('<NavigationItems />', () => {
+	let wrapper;
+	beforeEach(() => {
+		//render without deeply render the sub components
+		wrapper = shallow(<NavigationItems />);
+	});
 
 	//this is a unit test
 	it('should render two <NavigationItem> elements if not authenticated', () => {
-		//render without deeply render the sub components
-		const wrapper = shallow(<NavigationItems />);
-
-		//what we whant to find
+		
+		//what we whant to find using jest
 		expect(wrapper.find(NavigationItem)).toHaveLength(2);
 	});
 
 
+	//this is a unit test
+	it('should render three <NavigationItem> elements if authenticated', () => {
+		//pass props to wrapper
+		wrapper.setProps({isAuth: true});
+
+		//what we whant to find
+		expect(wrapper.find(NavigationItem)).toHaveLength(3);
+	});
+
+	//this is a unit test
+	it('should an exact logout button', () => {
+		//pass props to wrapper
+		wrapper.setProps({isAuth: true});
+
+		//what we whant to find
+		expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+	});
 });
